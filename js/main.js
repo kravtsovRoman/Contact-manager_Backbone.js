1e095aed89de3a722f8d92ee538a68a31a851e55
Roman <rozario91208@gmail.com>
2018-09-05 19:19:27 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -3,20 +3,5 @@ var Person = Backbone.Model.extend({
     name: 'Roma',
     age: '31',
     job: 'Frontend'
-  },
-
-  walk: function() {
-    return this.get('name') + 'is walking';
-  },
-
-  validate: function(attrs) {
-    console.log(attrs);
-    if (attrs.age <= 0) {
-      return 'Djphfcn yt vj;tn ,snm vtymit';
-    }
   }
 });
-
-var person = new Person();
-
-person.set('age', -25);
