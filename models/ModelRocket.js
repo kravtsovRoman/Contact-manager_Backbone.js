e9d02c70f4190c250f07540ad8a6f2447c95b897
Roman <rozario91208@gmail.com>
2018-09-06 16:12:27 +0300

    ASDA

--- models/ModelRocket.js
+++ models/ModelRocket.js
@@ -14,4 +14,14 @@ var RocketModel = Backbone.Model.extend({
   }
 });
 
-var RocketsCollection = Backbone.Collection.extend({});
+var RocketsCollection = Backbone.Collection.extend({
+  model: Model,
+  sortParam: 'size';
+  sortMode: 1,
+  comparator: function (a, b) {
+    if(a.get(this.sortParam) > b.get(this.sortParam)) return -1 * this.sortMode;
+    if(a.get(this.sortParam) < b.get(this.sortParam)) return this.sortMode;
+    return 0;
+  }
+  }
+});
--- views/ViewRocket.js
+++ views/ViewRocket.js
@@ -1,9 +1,45 @@
 var RocketView = Backbone.View.extend({
   tagName: 'tr',
 
-  events: {},
+  events: {
+    'click .changeSize': 'changeSize',
+    'click .deleteRow': 'deleteRow',
+    'blur .desc, .name, .size': 'editValue'
+  },
 
-  initialize: function() {},
+  initialize: function() {
+    this.template = _.template($('#viewRocket').html());
+    this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+  },
 
-  render: function() {}
+  render: function() {
+    var view = this.template(this.model.toJSON());
+    this.$el.html(view);
+  },
+
+  deleteRow: function() {
+    this.model.destroy();
+  },
+
+  editValue: function() {
+    var res = this.model.set(
+      {
+        name: this.$('.name').text(),
+        description: this.$('.desc').text(),
+        size: parseInt(this.$('input.size').attr('value'))
+      },
+      { validate: true }
+    );
+    if (!res) this.render();
+  },
+
+  changeSize: function(e) {
+    var diff = parseInt($(e.target).attr('data-rel'));
+    var size = this.model.get('size');
+    var res = this.model.set({
+      size: size + diff
+    });
+    if (!res) this.render();
+  }
 });
